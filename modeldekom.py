ed0b78c04cb144490d71a73a840e90b616f1f653
IbHansen <43575358+IbHansen@users.noreply.github.com>
2019-11-07 15:54:06 +0100

    attribution gui

---
+++ modeldekom.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+"""
+Module for making attribution analysis of a model. 
+
+The main function is attribution 
+
+Created on Wed May 31 08:50:51 2017
+
+@author: hanseni
+
+"""
+
+
+import pandas as pd
+import fnmatch 
+import matplotlib.pyplot as plt 
+import matplotlib as mpl
+import numpy
+
+
+
+from modelclass import ttimer 
+import modelclass as mc 
+import modeldekom as mk 
+import modelvis as mv
+
+idx= pd.IndexSlice
+    
+def attribution(model,experiments,start='',end='',save='',maxexp=10000,showtime=False,
+                summaryvar=['*']
+                ,silent=False,msilent=True):
+    """ Calculates an attribution analysis on a model 
+    accepts a dictionary with experiments. the key is experiment name, the value is a list 
+    of variables which has to be reset to the values in the baseline dataframe. """  
+    summaryout = model.vlist(summaryvar)
+    adverseny = model.lastdf
+    base = model.basedf
+    adverse0=adverseny[summaryout].loc[start:end,:].copy() 
+    ret={}
+    modelsave = model.save  # save the state of model.save 
+    model.save = False      # no need to save the experiments in each run 
+    with ttimer('Total dekomp',showtime):
+        for i,(e,var) in enumerate(experiments.items()):
+            if i >= maxexp : break     # when we are testing 
+            temp=adverseny[var].copy()
+            if not silent:
+                print(i,'Experiment :',e,'\n','Touching: \n', var)
+            adverseny[var] = base[var]
+            ret[e] = model(adverseny   ,start,end,samedata=True,
+                silent=msilent)[summaryout].loc[start:end,:]
+            adverseny[var] = temp
+#            adverseny = mc.upddf(adverseny,temp)
+
+        difret = {e : adverse0-ret[e]           for e in ret}
+    
+    df = pd.concat([difret[v] for v in difret],keys=difret.keys()).T
+    if save:
+         df.to_pickle('data\\' +save +r'.pc')    
+         
+    model.save = modelsave # restore the state of model.save 
+    return df
+
+
+def ilist(df,pat):
+    '''returns a list of variable in the model matching the pattern, 
+    the pattern can be a list of patterns of a sting with patterns seperated by 
+    blanks
+    
+    This function operates on the index names of a dataframe. Relevant for attribution analysis
+    '''
+    if isinstance(pat,list):
+           upat=pat
+    else:
+           upat = [pat]
+           
+    ipat = upat
+    out = [v for  p in ipat for up in p.split() for v in sorted(fnmatch.filter(df.index,up.upper()))]  
+    return out
+
+    
+def GetAllImpact(impact,sumaryvar):
+    ''' get all the impact from at impact dataframe''' 
+    exo = list({v for v,t in impact.columns})
+    df = pd.concat([impact.loc[sumaryvar,c]  for c in exo],axis=1)
+    df.columns = exo
+    return df 
+
+def GetSumImpact(impact,pat='PD__*'):
+    """Gets the accumulated differences attributet to each impact group """ 
+    a = impact.loc[ilist(impact,pat),:].groupby(level=[0],axis=1).sum()   
+    return a 
+
+def GetLastImpact(impact,pat='RCET1__*'):
+    """Gets the last differences attributet to each impact group """ 
+    a = impact.loc[ilist(impact,pat),:].groupby(level=[0],axis=1).last()   
+    return a
+ 
+def GetAllImpact(impact,pat='RCET1__*'):
+    """Gets the last differences attributet to each impact group """ 
+    a = impact.loc[ilist(impact,pat),:] 
+    return a 
+
+def GetOneImpact(impact,pat='RCET1__*',per=''):
+    """Gets differences attributet to each impact group in period:per """ 
+    a = impact.loc[ilist(impact,pat),idx[:,per]] 
+    a.columns = [v[0] for v in a.columns]
+    return a 
+
+def AggImpact(impact):
+    """ Calculates the sum of impacts and place in the last column
+    
+    This function is applied to the result iof a Get* function""" 
+    asum= impact.sum(axis=1)
+    asum.name = '_Sum'
+    aout = pd.concat([impact,asum],axis=1)
+    return aout 
+
+
+class totdekomp():
+    ''' Class to make modelvide attribution analysis 
+    
+    '''
+    
+    def __init__(self, model,summaryvar='*',desdic={} ):
+       
+       self.diffdf  = model.exodif()
+       self.diffvar = self.diffdf.columns
+       if len(self.diffvar) == 0:
+           print('No variables to attribute to ')
+           self.go = False 
+       else: 
+           self.go = True 
+           self.experiments = {v:v for v in self.diffvar}
+           self.model = model 
+           self.start = self.model.current_per.tolist()[0]
+           self.end = self.model.current_per.tolist()[-1]
+           
+           self.desdic = desdic 
+           self.summaryvar = summaryvar
+           self.summaryout = model.vlist(self.summaryvar)
+           
+           self.res = attribution(self.model,self.experiments,self.start,self.end,summaryvar=self.summaryvar,showtime=1,silent=1)
+       
+    def explain_last(self,pat='',top=0.9,title='Attribution last period'):  
+        if self.go:         
+            self.impact = mk.GetLastImpact(self.res,pat=pat).T
+            fig = mv.waterplot(self.impact,autosum=1,allsort=1,top=top,title= title,desdic=self.desdic)
+            return fig
+   
+    def explain_sum(self,pat='',top=0.9,title='Attribution, sum over all periods'): 
+        if self.go:          
+           self.impact = mk.GetSumImpact(self.res,pat=pat).T
+           fig = mv.waterplot(self.impact,autosum=1,allsort=1,top=top,title=title,desdic=self.desdic )
+           return fig
+   
+    def explain_per(self,pat='',per='',top=0.9,title='Attribution, for one periode'):   
+        if self.go:        
+           tper = self.res.columns.get_level_values(1)[0] if per == '' else per
+           self.impact = mk.GetOneImpact(self.res,pat=pat,per=tper).T
+           ntitle = f'{title}: {tper}'
+           fig = mv.waterplot(self.impact,autosum=1,allsort=1,top=top,title=ntitle,desdic=self.desdic )
+           return fig
+   
+            
+    def explain_all(self,pat='',stacked=True,kind='bar',top=0.9,title='Attribution'): 
+        if self.go:
+            selected =   GetAllImpact(self.res,pat) 
+            grouped = selected.stack().groupby(level=[0])
+            fig, axis = plt.subplots(nrows=len(grouped),ncols=1,figsize=(10,5*len(grouped)),constrained_layout=False)
+            width = 0.5  # the width of the barsser
+            laxis = axis if isinstance(axis,numpy.ndarray) else [axis]
+            for i,((name,dfatt),ax) in enumerate(zip(grouped,laxis)):
+                dfatt.index = [i[1] for i in dfatt.index]
+                dfatt.plot(ax=ax,kind=kind,stacked=stacked,title=self.desdic.get(name,name))
+                ax.set_ylabel(name,fontsize='x-large')
+                ax.set_xticklabels(dfatt.index.tolist(), rotation = 45,fontsize='x-large')
+            fig.suptitle(title,fontsize=20)
+            if 1:
+                plt.tight_layout()
+                fig.subplots_adjust(top=top)
+        
+            return fig
+
+if __name__ == '__main__' :
+    # running withe the mtotal model 
+    if  ( not 'mtotal' in locals() ) or True:
+        # get the model  
+        with open(r"models\mtotal.fru", "r") as text_file:
+            ftotal = text_file.read()
+       
+        #get the data 
+        base0   = pd.read_pickle(r'data\base0.pc')    
+        base    = pd.read_pickle(r'data\base.pc')    
+        adve0   = pd.read_pickle(r'data\adve0.pc')      
+    #%%    
+        mtotal  = mc.model(ftotal)
+        
+#        prune(mtotal,base)
+        #%%    
+        baseny        = mtotal(base0   ,'2016q1','2018q4',samedata=False)
+        adverseny     = mtotal(adve0   ,'2016q1','2018q4',samedata=True)
+    #%%
+    diff  = mtotal.exodif()   # exogeneous variables which are different between baseny and adverseny 
+    #%%
+    assert 1==2  # just for stopping in test situations 
+    #%%  
+    adverseny            = mtotal(adve0   ,'2016q1','2018q4',samedata=True)   # to makew sure we have the right adverse.
+    countries            = {c.split('__')[2]  for c in diff.columns}  # list of countries 
+    countryexperiments   = {e: [c for c in diff.columns if ('__'+e+'__') in c]   for e in countries } # dic of experiments 
+    assert len(diff.columns) == sum([len(c) for c in countryexperiments.values()]) , 'Not all exogeneous chocks variables are accountet for'
+    countryimpact        = attribution(mtotal,countryexperiments,save='countryimpactxx',maxexp=30000,showtime = 1)    
+    #%% 
+    adverseny     = mtotal(adve0   ,'2016q1','2018q4',samedata=True)
+    vartypes             = {c.split('__') [1]  for c in diff.columns}
+    vartypeexperiments   = {e: [c for c in diff.columns if ('__'+e+'__') in c]   for e in vartypes }
+    assert len(diff.columns) == sum([len(c) for c in vartypeexperiments.values()]) , 'Not all exogeneous chocks variables are accountet for'
+    vartypeimpact        = attribution(mtotal,vartypeexperiments,save='vartypeimpactxx',maxexp=3000,showtime=1)
+    ##%%
+    #adverseny     = mtotal(adve0   ,'2016q1','2018q4',samedata=True)
+    #allexo                  = {c[7:14] for c in diff.columns} 
+    #allexoexperiments       = {e: [c for c in diff.columns if ('__'+e+'__') in c]   for e in allexo }
+    #allexoimpact            = attribution(mtotal,allexoexperiments,base,adverseny,save='allexoimpact',maxexp=2000)
+#%% test of upddf    
+    if 0:
+        baseny        = mtotal(base0   ,'2016q1','2018q4',samedata=False)
+        adverseny     = mtotal(adve0   ,'2016q1','2018q4',samedata=True)
+#%%
+        e = 'EE'     
+        var = countryexperiments['EE']
+        vardiff = diff[var]
+        temp = adverseny[var].copy()
+        adverseny[var] = baseny[var]
+        temp2 = adverseny[var].copy()
+        _ = mc.upddf(adverseny,temp) 
+        adverseny[var] = temp
+        temp3 = adverseny[var].copy()
+        
---
+++ modeljupyter.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 21:26:13 2019
+
+@author: hanseni
+"""
+
+import pandas as pd
+import  ipywidgets as widgets  
+from IPython.display import display, clear_output
+import matplotlib.pylab  as plt 
+import seaborn as sns
+#import qgrid 
+
+import sys
+sys. path.append('modelflow/')
+ 
+
+def vis_alt3(dfs,model,title='Show variables',basename='Baseline',altname='Alternative',trans={}):
+    ''' display tabbed widget with results from different dataframes, usuallly 2 but more can be shown
+    
+    dfs is a list of dataframes. They should be of same dimensionalities 
+    
+    '''
+    avar = dfs[0].columns
+    outlist =     [widgets.Output() for var in avar]
+    outdiflist =  [widgets.Output() for var in avar]    
+    deplist =     [widgets.Output() for var in avar]
+    reslist =     [widgets.Output() for var in avar]
+    attlist =     [widgets.Output() for var in avar]
+    varouttablist =  [widgets.Tab(children = [out,outdif,att,dep,res]) 
+                      for out,outdif,att,dep,res  in zip(outlist,outdiflist,attlist,deplist,reslist)]
+    for var,varouttab in zip(avar,varouttablist):
+        for i,tabtext in enumerate(['Level','Change','Attribution','Dependencies','Results']):
+            varouttab.set_title(i,tabtext)
+
+    controllist = [widgets.VBox([varouttab]) for varouttab in varouttablist]
+    tab = widgets.Tab(children = controllist)
+    for i,(var,out) in enumerate(zip(avar,controllist)):
+        tab.set_title(i, var) 
+    
+    def showvar(b):
+        sel = b['new']
+        out = outlist[sel]
+        outdif = outdiflist[sel]
+        dep = deplist[sel]
+        res = reslist[sel]
+        att = attlist[sel]
+        var = avar[sel]
+        with out:
+            clear_output()
+            fig,ax = plt.subplots(figsize=(10,6))
+            ax.set_title(trans.get(var,var),fontsize=14)
+            ax.spines['right'].set_visible(False)
+#            ax.spines['top'].set_visible(False)
+            for i,df in enumerate(dfs):
+                data = df.loc[:,var]
+                data.plot(ax=ax,legend=False,fontsize=14)
+                x_pos = data.index[-1]
+                if i == 0:
+                    basevalue = data.values[-1]
+                else:
+                    alt=i if len(dfs) >= 3 else ''
+                    if (abs(data.values[-1]-basevalue) < 0.01 or 
+                      (abs(basevalue) > 100. and (1.-abs(data.values[-1]/basevalue) < 0.008))):
+                        if i == 1:
+                            ax.text(x_pos, data.values[-1]  ,f' {basename} and {altname}{alt}',fontsize=14)
+                    else:
+                        ax.text(x_pos, data.values[-1]  ,f' {altname}{alt}',fontsize=14)
+                        ax.text(x_pos, basevalue  ,f' {basename}',fontsize=14)
+            plt.show(fig)
+        with outdif:
+            clear_output()
+            fig,ax = plt.subplots(figsize=(10,6))
+            ax.set_title(trans.get(var,var),fontsize=14)
+            ax.spines['right'].set_visible(False)
+            for i,df in enumerate(dfs):
+                if i == 0:
+                    basedata = df.loc[:,var]
+                    x_pos = data.index[-1]
+                else:
+                    data = df.loc[:,var]-basedata
+                    data.plot(ax=ax,legend=False,fontsize=14)
+                    x_pos = data.index[-1]
+                    alt=i if len(dfs) >= 3 else ''
+                    ax.text(x_pos, data.values[-1]  ,f' Impact of {altname}{alt}',fontsize=14)
+            plt.show(fig)
+        with dep:
+            clear_output()
+            model.draw(var,up=2,down=2,svg=1)
+        with res:
+            clear_output()
+            out = model.get_values(var).T.rename(columns={'Base':basename,'Last':altname})
+            out.style.set_caption(trans.get(var,var))
+            print(trans.get(var,var))
+            print(out)
+        with att:
+            clear_output()
+            #model.smpl(N-20,N)
+            if var in model.endogene:
+                print(f'What explains the difference between the {basename} and the {altname} run ')
+                print(model.allvar[var]['frml'])
+                model.explain(var,up=0,dec=1,size=(9,12),svg=1,HR=0)
+            else:
+                print(f'{var} is exogeneous and attribution can not be performed')
+            
+    display(tab) 
+
+    showvar({'new':0})
+    tab.observe(showvar,'selected_index')
+    return tab
+
+# Define data extraction
+def get_alt(mmodel,pat):
+    ''' Retrieves variables matching pat from a model '''
+    varnames = mmodel.vlist(pat)
+    modelvar = mmodel.exogene | mmodel.endogene
+    modelvarnames = list(dict.fromkeys([v for v in varnames if v in modelvar])) # to awoid dublicate names
+    per = mmodel.current_per
+    return [mmodel.basedf.loc[per,modelvarnames],mmodel.lastdf.loc[per,modelvarnames]]
+
+
+
+
+
+def inputwidget(model,df,slidedef={},radiodef=[],checkdef=[],modelopt={},varpat='RFF XGDPN RFFMIN GFSRPN DMPTRSH XXIBDUMMY'
+                 ,showout=1,trans={}):
+    '''Creates an input widgets for updating variables 
+    
+    :df: Baseline dataframe 
+    :slidedef: dict with definition of variables to be updated by slider
+    :radiodef: dict of lists. each at first level defines a collection of radiobuttoms
+               second level defines the text for each leved and the variable to set or reset to 0
+    :varpat: the variables to show in the output widget
+    :showout: 1 if the output widget is to be called '''
+    
+    lradiodef= len(radiodef)
+    lslidedef = len(slidedef)
+    lcheckdef = len(checkdef)
+    basename ='Baseline'
+    
+    if lradiodef: 
+        wradiolist = [widgets.RadioButtons(options=[i for i,j in cont],description=des,layout={'width':'70%'},
+                                           style={'description_width':'37%'}) for des,cont in radiodef.items()]
+        if len(wradiolist) <=2:
+            wradio = widgets.HBox(wradiolist)
+        else: 
+            wradio = widgets.VBox(wradiolist)
+
+            
+
+# define slidesets 
+    if lslidedef:     
+        wexp  = widgets.Label(value="Input new parameter ",layout={'width':'41%'})
+        walt  = widgets.Label(value="Alternative",layout={'width':'8%'})
+        wbas  = widgets.Label(value="Baseline",layout={'width':'8%'})
+        whead = widgets.HBox([wexp,walt,wbas])
+        
+        wset  = [widgets.FloatSlider(description=des,
+                                    min=cont['min'],max=cont['max'],value=cont['value'],step=cont.get('step',0.01),
+                                    layout={'width':'60%'},style={'description_width':'40%'})
+                 for des,cont in slidedef.items()]
+        waltval= [widgets.Label(value=f"{cont['value']:>.2f}",layout={'width':'8%'})
+                  for des,cont  in slidedef.items()]
+        wslide = [widgets.HBox([s,v]) for s,v in zip(wset,waltval)]
+       
+# cheklist  
+    if lcheckdef:         
+        wchecklist = [widgets.Checkbox(description=des,value=val)   for des,var,val in checkdef]
+        wcheck  = widgets.HBox(wchecklist)   
+
+# some buttons and text     
+    wname = widgets.Text(value='Alternative',placeholder='Type something',description='Name of experiment:',
+                        layout={'width':'30%'},style={'description_width':'50%'})
+    wpat = widgets.Text(value=varpat,placeholder='Type something',description='Output variables:',
+                        layout={'width':'65%'},style={'description_width':'30%'})
+    winputstring = widgets.HBox([wname,wpat])
+    
+    wgo   = widgets.Button(description="Run")
+    wreset   = widgets.Button(description="Reset to default")
+    wsetbas   = widgets.Button(description="Use as baseline")
+    wbut  = widgets.HBox([wgo,wreset,wsetbas])
+    
+    wvar = [whead]+wslide if lslidedef else []
+    if lradiodef: wvar = wvar + [wradio]
+    if lcheckdef: wvar = wvar + [wcheck]
+        
+    w     = widgets.VBox(wvar+[winputstring] +[wbut])
+
+    # This function is run when the button is clecked 
+    def run(b):
+        mulstart       = model.basedf.copy()
+        
+        # First update from the sliders 
+        if lslidedef:
+            for i,(des,cont) in enumerate(slidedef.items()):
+                op = cont.get('op','=')
+                var = cont['var']
+                for var in cont['var'].split():
+                    if  op == '+':
+                        mulstart.loc[model.current_per,var]    =  mulstart.loc[model.current_per,var] + wset[i].value
+                    elif op == '+impulse':    
+                        mulstart.loc[model.current_per[0],var] =  mulstart.loc[model.current_per[0],var] + wset[i].value
+                    elif op == '=':    
+                        mulstart.loc[model.current_per,var] =   wset[i].value
+                    elif op == '=impuse':    
+                        mulstart.loc[model.current_per,var] =   wset[i].value
+                    else:
+                        print(f'Wrong operator in {cont}.\nNot updated')
+                        
+                
+        # now  update from the radio buttons 
+        if lradiodef:
+            for wradio,(des,cont) in zip(wradiolist,radiodef.items()):
+                print(des,wradio.value,wradio.index,cont[wradio.index])
+                for v in cont:
+                    mulstart.loc[model.current_per,v[1]] = 0.0
+                mulstart.loc[model.current_per,cont[wradio.index][1]] = 1.0  
+ 
+        if lcheckdef:           
+            for box,(des,var,_) in zip(wchecklist,checkdef):
+                mulstart.loc[model.current_per,var] = 1.0 * box.value
+
+        #with out:
+        clear_output()
+        mul = model(mulstart,**modelopt)
+
+        clear_output()
+        display(w)
+        #_ = mfrbus['XGDPN RFF RFFMIN GFSRPN'].dif.rename(trans).plot(colrow=1,sharey=0)
+        if showout:
+            a = vis_alt3(get_alt(model,wpat.value),model,basename=basename,altname=wname.value,trans=trans)
+
+    def reset(b):
+        if lslidedef:
+            for i,(des,cont) in enumerate(slidedef.items()):
+                wset[i].value  =   cont['value']
+            
+        if lradiodef:
+            for wradio in wradiolist:
+                wradio.index = 0
+            
+        if lcheckdef:           
+            for box,(des,var,defvalue) in zip(wchecklist,checkdef):
+                box.value = defvalue
+
+    def setbas(b):
+        nonlocal basename
+        model.basedf = model.lastdf.copy(deep=True)
+        basename = wname.value
+        
+    # Assign the function to the button  
+    wgo.on_click(run)
+    wreset.on_click(reset)
+    wsetbas.on_click(setbas)
+    out = widgets.Output()
+    
+    return w
